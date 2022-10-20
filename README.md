d0a6ff7a6cd7c88177ba0f5c8e03fdf86e3386c9
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-10-20 16:35:14 +0300

    setup, vite + react

---
+++ README.md
@@ -0,0 +1,4 @@
+In acest proiect voi creea un site care va ajuta pacientii sa isi gasesca in zona lor de interes, un medic de o anumita specializare.
+
+Pacientul va putea verifica detalii despre medic incluzand posibilitatea de a face o programare direct pe site sau trimitand un mail instant.
+
---
+++ project+vite/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App(){
+  // Navbar 
+  // Container 
+  //   Bara de cautare 
+  //   Optiune sa selectezi orasul si specializarea
+  // Container pentru rezultate
+  //   Detaliile medicului 
+  //   Date de contact 
+  //   Optine sa te programezi prin site, se deschide calendar unde poate selecta ziua si ora 
+  //   Optiune de trimitere mail instant cu un form
+
+}
+
+export default App
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div className="App">
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src="/vite.svg" className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://reactjs.org" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </div>
+//   )
+// }

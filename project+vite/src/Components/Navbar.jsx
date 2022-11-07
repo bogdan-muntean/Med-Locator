4eb60bf822fb4a323ab4e70116d67cba9a6f0e55
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-11-07 10:37:06 +0200

    edit navbar and search-bar - dom and style

--- project+vite/src/Components/Navbar.jsx
+++ project+vite/src/Components/Navbar.jsx
@@ -1,19 +1,43 @@
 import React from "react";
 import medicalLogo from "./../images/medical-logo.jpg";
+// import { faXmark } from  '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
-    <div className="container-navbar grayscale">
-      <img src={medicalLogo}></img>
-      <div>
-        <a src="#">Acasa</a>
-        <a src="#">Noutati</a>
-        <a src="#">Inregistrare</a>
-        <a src="#">Logare</a>
-        <a src="#">Cumpara</a>
+    <div className="container-navbar">
+      <div className="top-area">
+        <div className="row">
+          <div className="col1">
+            <div className="top-email">
+            {/* <FontAwesomeIcon icon="fa-regular fa-envelope" /> */}
+              <a href="mailto:info@yourspecialist.com">info@yourspecialist.com</a>
+            </div>
+                        
+            <div className="top-phone">
+              <a href="tel:0264948299">0264948299</a>
+            </div>
+          </div>
+          <div className="col2" id="div-col2">
+              <div>
+                <a href="#">Creaza cont nou sau Conecteaza-te</a>
+              </div>
+          </div>
+        </div>
       </div>
+      <nav>
+        <label className="logo">YourSpecialist</label>
+        <ul>
+          <li><a href="#">Acasa</a></li>
+          <li><a href="#">Despre noi</a></li>
+          <li><a href="#">Spitale si Clinici</a></li>
+          <li><a href="#">Contacte</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+      </nav>
     </div>
+    
   );
 };
 
 export default Navbar;
+
--- project+vite/src/Components/SearchBar.jsx
+++ project+vite/src/Components/SearchBar.jsx
@@ -1,28 +1,68 @@
-import React from 'react'
-import background from "../images/doctor-work.jpg"
+import React from "react";
+import background from "../images/doctor-work.jpg";
 import background2 from "./../images/medical-orizontal1.jpg";
 import background3 from "./../images/medical-image1.jpg";
 import background4 from "./../images/search-banner.jpg";
 
 const SearchBar = () => {
   return (
-    <div className='container-search-bar' 
-    style={{backgroundImage: `url(${background4})`}}
+    <div
+      className="container-search-bar"
+      style={{ backgroundImage: `url(${background4})` }}
     >
-        <div className='search-bar'>
-            <h3>Cauta personalul medical dupa urmatoarele criterii</h3>
-            <form>
-                <select name="oras" id="oras">
-                    <option name="bucuresti" id="bucuresti">Bucuresti</option>
-                    <option name="cluj-napoca" id="cluj-napoca">Cluj-Napoca</option>
-                    <option name="brasov" id="brasov">Brasov</option>
-                    <option name="timisoara" id="timisoara">Timisoara</option>
-                </select>  
-            </form>
+      <div className="info-text">
+        <p>
+          Bine ai venit sa gasesti un doctor.<br></br>
+          <span>
+            Doctori recomandati. Alegeri informate.<br></br>
+            Trimite Email cu <a href="#">INTREBARILE</a> catre YourSpecialist.<br></br>
+          </span>
+        </p>
+        <div>
+          <span>Creaza un cont pe YourSpecialist sau Logheaza-te acum</span>
         </div>
-
+        <div>
+          <span>Locuri de munca pentru doctori</span>
+        </div>
+      </div>
+      <div className="search-doctor">
+        <h3>Cauti un doctor/dentist?</h3>
+        <h1>Cauti un doctor/dentist?</h1>
+        <form>
+          <label for="specialist">Caut un*</label>
+          <select id="specialist" name="specialist">
+            <option value="none">Specialist...</option>
+            <option value="doctor">Doctor</option>
+            <option value="dentist">Dentist</option>
+          </select>
+          <label for="oras">Oras</label>
+          <select id="oras" name="oras">
+            <option value="none">Oras...</option>
+            <option value=""></option>
+            <option value=""></option>
+          </select>
+          <label for="specialitate">Specialitate</label>
+          <select id="specialitate" name="specialitate">
+            <option value="none">Alege specialitatea...</option>
+            <option value=""></option>
+            <option value=""></option>
+          </select>
+          <label for="prenume">Prenume</label>
+          <input id="prenume" name="prenume"></input>
+          <label for="nume">Nume</label>
+          <input id="nume" name="nume"></input>
+          
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
+
+{
+
+
+
+
+}
